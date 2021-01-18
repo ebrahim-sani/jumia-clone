acaf4e90ad23a75af0403f40281b44d768469fe6
ebrahim-sani <ibraheemsani035@gmail.com>
2021-01-18 16:50:07 +0100

    Add firebase

---
+++ jumia/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD-ohcXfZHp6tF3usjLw6oSA_NTz3D4m7U",
+  authDomain: "jumia-clone-c495f.firebaseapp.com",
+  projectId: "jumia-clone-c495f",
+  storageBucket: "jumia-clone-c495f.appspot.com",
+  messagingSenderId: "851371926928",
+  appId: "1:851371926928:web:965726d2cf0cb8be5dbf15",
+};
